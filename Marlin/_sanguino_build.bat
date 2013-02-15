8c4a53c327883f20185914b21ac485e4b6c48935
Yuri Valentini <yuroller@gmail.com>
2013-02-15 17:28:25 +0100

    Changed paths to build scripts

--- Marlin/_sanguino_build.bat
+++ Marlin/_sanguino_build.bat
@@ -1,7 +1,7 @@
 @echo off
 set _old_path=%PATH%
-set path=%PATH%;C:\UserProgramsX86\arduino-1.0.3\hardware\tools\avr\utils\bin
+set path=%PATH%;C:\UserPrograms\arduino-1.0.3\hardware\tools\avr\utils\bin
 make clean
-make HARDWARE_MOTHERBOARD=62 AVR_TOOLS_PATH=C:\UserProgramsX86\arduino-1.0.3\hardware\tools\avr\bin\ ARDUINO_VERSION=103
+make HARDWARE_MOTHERBOARD=62 AVR_TOOLS_PATH=C:\UserPrograms\arduino-1.0.3\hardware\tools\avr\bin\ ARDUINO_VERSION=103
 set path=%_old_path%;
 set _old_path=
