0dd960347ccae0ac589ff0e3ba04eb7c4487adf1
Yuri Valentini <yuroller@gmail.com>
2015-08-01 19:44:45 +0200

    converted script to unix environment

---
+++ Marlin/_dump_flash1284.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+AVR_TOOLS_DIR=~/opt/arduino-1.6.5/hardware/tools/avr
+${AVR_TOOLS_DIR}/bin/avrdude -C ${AVR_TOOLS_DIR}/etc/avrdude.conf -b 57600 -c arduino -p m1284p -P /dev/ttyUSB0 -U flash:r:flashdump.hex:i
---
+++ Marlin/_sanguino_build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+make HARDWARE_MOTHERBOARD=62 AVR_TOOLS_PATH=~/opt/arduino-1.6.5/hardware/tools/avr/bin/ ARDUINO_VERSION=105
